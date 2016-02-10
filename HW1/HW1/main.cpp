45e34ccd8ee2dacd40fdd2cb15350f750a0cbbfb
tostii <zheng.y.simon@gmail.com>
2016-02-09 23:03:42 -0500

    modular code + HW2

--- HW1/HW1/main.cpp
+++ HW1/HW1/main.cpp
@@ -5,7 +5,37 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <time.h>
 
+
+//generic monte carlo simulation that takes the method used as a parameter
+double optionMC(double spot, double strike, double r, double expiry, double vol, double yield, double numPath, double numStep, std::string type, double (*f)(double, double, double, double, double, double, double)){	
+	double runningSum = 0;
+	for (unsigned long j = 0; j < numPath; j++) {
+		double iterateSpot = spot;
+		for (int i = 1; i <= numStep; i++) {
+			iterateSpot = f(iterateSpot, strike, r, expiry, vol, yield, numStep);
+		}
+		double thisPayoff;
+		if (type == "put") {
+			thisPayoff = strike - iterateSpot;
+		}
+		else if (type == "call") {
+			thisPayoff = iterateSpot - strike;
+		}
+		else {
+			return -1;
+		}
+		thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
+		runningSum += thisPayoff;
+	}
+	double mean = runningSum / numPath;
+
+	mean *= exp(-r*expiry);
+	return mean;
+}
+
+//closed-form
 double closedPut(double spot, double strike, double r, double expiry, double vol, double yield, std::string type) {
 	double d1 = (log(spot / strike) + (r - yield + pow(vol, 2) / 2)*expiry) / (vol * sqrt(expiry));
 	double d2 = d1 - vol*sqrt(expiry);
@@ -20,122 +50,46 @@ double closedPut(double spot, double strike, double r, double expiry, double vol
 	}
 }
 
-double analytical(double spot, double strike, double r, double expiry, double vol, double yield, double numPath, std::string type) {
-	double var = vol*vol*expiry;
-	double rootVar = sqrt(var);
-	double itoCorrection = 0.5*var;
-
-	double movedSpot = spot*exp(r*expiry - itoCorrection - yield);
-	double thisSpot;
-	double runningSum = 0;
-
-	for (unsigned long i = 0; i < numPath; i++) {
-		double thisGaussian = GetOneGaussianByBoxMuller();
-		thisSpot = movedSpot*exp(rootVar*sqrt(expiry)*thisGaussian);
-		double thisPayoff;
-		if (type == "put") {
-			thisPayoff = strike - thisSpot;
-		}
-		else if (type == "call") {
-			thisPayoff = thisSpot - strike;
-		}
-		else {
-			return -1;
-		}
-		thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
-		runningSum += thisPayoff;
-	}
-	double mean = runningSum / numPath;
-	mean *= exp(-r*expiry);
-	return mean;
+//analytical one step
+double analytical(double spot, double strike, double r, double expiry, double vol, double yield, double numStep) {
+	return spot*exp(vol*expiry*GetOneGaussianByBoxMuller());
 }
 
-double euler(double spot, double strike, double r, double expiry, double vol, double yield, double numPath, int steps, std::string type) {
-	double dt = 1 / static_cast<double>(steps);
-	double runningSum = 0;
-	for (unsigned long j = 0; j < numPath; j++) {
-		double iterateSpot = spot;
-		for (int i = 1; i <= steps; i++) {
-			double dW = sqrt(dt) * GetOneGaussianByBoxMuller();
-			double dS = iterateSpot*((r - yield)*dt + vol*dW);
-			iterateSpot = iterateSpot + dS;
-		}
-		double thisPayoff;
-		if (type == "put") {
-			thisPayoff = strike - iterateSpot;
-		}
-		else if (type == "call") {
-			thisPayoff = iterateSpot - strike;
-		}
-		else {
-			return -1;
-		}
-		thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
-		runningSum += thisPayoff;
-	}
-	double mean = runningSum / numPath;
-	mean *= exp(-r*expiry);
-	return mean;
+//euler
+double euler(double spot, double strike, double r, double expiry, double vol, double yield, double numStep) {
+	double dt = 1 / numStep;
+	double dW = sqrt(dt) * GetOneGaussianByBoxMuller();
+	double dS = spot*((r - yield)*dt + vol*dW);
+	return spot + dS;
 }
 
-double eulerLog(double spot, double strike, double r, double expiry, double vol, double yield, double numPath, int steps, std::string type) {
-	double dt = 1 / static_cast<double>(steps);
-	double runningSum = 0;
-	for (unsigned long j = 0; j < numPath; j++) {
-		double iterateSpot = log(spot);
-		for (int i = 1; i <= steps; i++) {
-			double dW = sqrt(dt) * GetOneGaussianByBoxMuller();
-			double dS = ((r - yield - pow(vol, 2) / 2)*dt + vol*dW);
-			iterateSpot = iterateSpot + dS;
-		}
-		iterateSpot = exp(iterateSpot);
-		double thisPayoff;
-		if (type == "put") {
-			thisPayoff = strike - iterateSpot;
-		}
-		else if (type == "call") {
-			thisPayoff = iterateSpot - strike;
-		}
-		else {
-			return -1;
-		}
-		thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
-		runningSum += thisPayoff;
+//euler log
+double eulerLog(double spot, double strike, double r, double expiry, double vol, double yield, double numStep) {
+	static int count = 1;
+	double dt = 1 / numStep;
+	double dW = sqrt(dt) * GetOneGaussianByBoxMuller();
+	double dS = ((r - yield - pow(vol, 2) / 2)*dt + vol*dW);
+	if (count == numStep) {
+		count = 1;
+		return exp(spot + dS);
+
 	}
-	double mean = runningSum / numPath;
-	mean *= exp(-r*expiry);
-	return mean;
+	else {
+		count++;
+		return spot + dS;
+	}
 }
 
-double milstein(double spot, double strike, double r, double expiry, double vol, double yield, double numPath, int steps, std::string type) {
-	double dt = 1 / static_cast<double>(steps);
-	double runningSum = 0;
-	for (unsigned long j = 0; j < numPath; j++) {
-		double iterateSpot = spot;
-		for (int i = 1; i <= steps; i++) {
-			double dW = sqrt(dt) * GetOneGaussianByBoxMuller();
-			double dS = iterateSpot*((r - yield)*dt + vol*dW + 0.5*vol*vol*(dW*dW - dt));
-			iterateSpot = iterateSpot + dS;
-		}
-
-		double thisPayoff;
-		if (type == "put") {
-			thisPayoff = strike - iterateSpot;
-		}
-		else if (type == "call") {
-			thisPayoff = iterateSpot - strike;
-		}
-		else {
-			return -1;
-		}
-		thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
-		runningSum += thisPayoff;
-	}
-	double mean = runningSum / numPath;
-	mean *= exp(-r*expiry);
-	return mean;
+//milstein
+double milstein(double spot, double strike, double r, double expiry, double vol, double yield, double numStep) {
+	double dt = 1 / numStep;
+	double dW = sqrt(dt) * GetOneGaussianByBoxMuller();
+	double dS = spot*((r - yield)*dt + vol*dW + 0.5*vol*vol*(dW*dW - dt));
+	return spot + dS;
 }
 
+
+//main function
 int main() {
 	//begin input
 	const double spot = 100;
@@ -144,83 +98,29 @@ int main() {
 	const double vol = 0.3;
 	const double yield = 0.02;
 	const double expiry = 1;
-	const int numSteps = 252;
-	const int numPath = 10000;
+	const double numStep = 252;
+	const double numPath = 10000;
+	srand(time(NULL));
 	//end input
-	int choice = 0;
-	while (choice != 3) {
-		int choice2 = 0;
-		std::cout << "European Style Option" << std::endl;
-		std::cout << "1. Call" << std::endl;
-		std::cout << "2. Put" << std::endl;
-		std::cout << "3. Exit" << std::endl;
-		std::cin >> choice;
-		switch (choice) {
-		case 1:
-			while (choice2 != 6) {
-				std::cout << "European Call Option: " << std::endl;
-				std::cout << "1. Closed-form formulae" << std::endl;
-				std::cout << "2. Analytical Solution" << std::endl;
-				std::cout << "3. Euler numerical solution" << std::endl;
-				std::cout << "4. Euler numerical solution for log spot" << std::endl;
-				std::cout << "5. Milstein numerical solution" << std::endl;
-				std::cout << "6. Back" << std::endl;
-				std::cin >> choice2;
-				switch (choice2) {
-				case 1:
-					std::cout << "Option price using closed-form formula: " << closedPut(spot, strike, r, expiry, vol, yield, "call") << std::endl;
-					break;
-				case 2:
-					std::cout << "Option price using single-step exact SDE solution: " << analytical(spot, strike, r, expiry, vol, yield, numPath, "call") << std::endl;
-					break;
-				case 3:
-					std::cout << "Option price using Euler numerical solution of SDE for spot: " << euler(spot, strike, r, expiry, vol, yield, numPath, numSteps, "call") << std::endl;
-					break;
-				case 4:
-					std::cout << "Option price using Euler numerical solution of SDE for log spot:5 " << eulerLog(spot, strike, r, expiry, vol, yield, numPath, numSteps, "call") << std::endl;
-					break;
-				case 5:
-					std::cout << "Option price using Milstein numerical solution of SDE for spot: " << milstein(spot, strike, r, expiry, vol, yield, numPath, numSteps, "call") << std::endl;
-					break;
-				default:
-					break;
-				}
-			}
-			break;
-		case 2:
-			while (choice2 != 6) {
-				std::cout << "European Put Option: " << std::endl;
-				std::cout << "1. Closed-form formulae" << std::endl;
-				std::cout << "2. Analytical Solution" << std::endl;
-				std::cout << "3. Euler numerical solution" << std::endl;
-				std::cout << "4. Euler numerical solution for log spot" << std::endl;
-				std::cout << "5. Milstein numerical solution" << std::endl;
-				std::cout << "6. Back" << std::endl;
-				std::cin >> choice2;
-				switch (choice2) {
-				case 1:
-					std::cout << "Option price using closed-form formula: " << closedPut(spot, strike, r, expiry, vol, yield, "put") << std::endl;
-					break;
-				case 2:
-					std::cout << "Option price using single-step exact SDE solution: " << analytical(spot, strike, r, expiry, vol, yield, numPath, "put") << std::endl;
-					break;
-				case 3:
-					std::cout << "Option price using Euler numerical solution of SDE for spot: " << euler(spot, strike, r, expiry, vol, yield, numPath, numSteps, "put") << std::endl;
-					break;
-				case 4:
-					std::cout << "Option price using Euler numerical solution of SDE for log spot: " << eulerLog(spot, strike, r, expiry, vol, yield, numPath, numSteps, "put") << std::endl;
-					break;
-				case 5:
-					std::cout << "Option price using Milstein numerical solution of SDE for spot: " << milstein(spot, strike, r, expiry, vol, yield, numPath, numSteps, "put") << std::endl;
-					break;
-				default:
-					break;
-				}
-			}
-			break;
-		default:
-			break;
-		}
-	}
+
+	std::cout << "Assuming that the stock price is geometric Brownian motion with volatility " << vol << ", initial asset price S(0) = " << spot << ", constant risk-free interest rate r = " << r << ", dividend yield = " << yield << ", strike price K = " << strike << ", and maturity T = " << expiry << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "European Call Option" << std::endl;
+	std::cout << "Option price using closed-form formula: " << closedPut(spot, strike, r, expiry, vol, yield, "call") << std::endl;
+	std::cout << "Option price using single-step exact SDE solution: " << optionMC(spot*exp(r*expiry - 0.5*vol*vol*expiry - yield), strike, r, expiry, vol, yield, numPath, 1, "call", (*analytical)) << std::endl;
+	std::cout << "Option price using Euler numerical solution of SDE for spot: " << optionMC(spot, strike, r, expiry, vol, yield, numPath, numStep, "call", (*euler)) << std::endl;
+	std::cout << "Option price using Euler numerical solutionn of SDE for log spot: " << optionMC(log(spot), strike, r, expiry, vol, yield, numPath, numStep, "call", (*eulerLog)) << std::endl;
+	std::cout << "Option price using Milstein numerical solution of SDE for spot: " << optionMC(spot, strike, r, expiry, vol, yield, numPath, numStep, "call", (*milstein)) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "European Put Option" << std::endl;
+	std::cout << "Option price using closed-form formula: " << closedPut(spot, strike, r, expiry, vol, yield, "put") << std::endl;
+	std::cout << "Option price using single-step exact SDE solution: " << optionMC(spot*exp(r*expiry - 0.5*vol*vol*expiry - yield), strike, r, expiry, vol, yield, numPath, 1, "put", (*analytical)) << std::endl;
+	std::cout << "Option price using Euler numerical solution of SDE for spot: " << optionMC(spot, strike, r, expiry, vol, yield, numPath, numStep, "put", (*euler)) << std::endl;
+	std::cout << "Option price using Euler numerical solutionn of SDE for log spot: " << optionMC(log(spot), strike, r, expiry, vol, yield, numPath, numStep, "put", (*eulerLog)) << std::endl;
+	std::cout << "Option price using Milstein numerical solution of SDE for spot: " << optionMC(spot, strike, r, expiry, vol, yield, numPath, numStep, "put", (*milstein)) << std::endl;
+	std::cin.get();
 	return 0;
+
 }
